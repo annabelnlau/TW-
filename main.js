db60276414222f76b6c32f4dbf7ceb6abe2bb692
Annabel Lau <annabel.n.lau@gmail.com>
2018-01-19 16:25:18 -0500

    created alert function

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-alert("Hey! This page may contain potentially triggering content. Do you want to continue?")
+let allElements = document.getElementsByTagName('*')
+let triggers = ['sexual assault', 'rape']
+let foundTrigger = false
+
+
+for (let i = 0; i < allElements.length; i++) {
+  let currentElement = allElements[i]
+  let children = currentElement.childNodes
+  for (let j = 0; j < children.length; j++) {
+    let child = children[j]
+
+    if (child.nodeType === 3) {
+      let text = child.nodeValue
+      if (triggers.indexOf(text.toLowerCase()) > -1) {
+        foundTrigger = true
+        break
+      }
+    }
+  }
+
+
+}
+
+if (foundTrigger) {
+  alert('Trigger Warning')
+}
